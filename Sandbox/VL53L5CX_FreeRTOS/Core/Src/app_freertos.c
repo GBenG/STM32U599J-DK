8757947815b33824418985f3bb4ed85af34a5f7a
Ben.G <sps.tech.way@gmail.com>
2024-02-01 23:08:02 +0200

    rid of printf warnings

--- Sandbox/VL53L5CX_FreeRTOS/Core/Src/app_freertos.c
+++ Sandbox/VL53L5CX_FreeRTOS/Core/Src/app_freertos.c
@@ -22,6 +22,7 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os2.h"
+#include <stdio.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "vl53l5cx_api.h"
