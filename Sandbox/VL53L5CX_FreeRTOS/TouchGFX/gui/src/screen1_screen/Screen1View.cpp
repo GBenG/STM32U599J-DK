3cbbe1694c1b6b65562b7cff4f1a24663a4e637d
Ben.G <sps.tech.way@gmail.com>
2024-02-01 23:45:25 +0200

    it works! from sensor to screen)

--- Sandbox/VL53L5CX_FreeRTOS/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ Sandbox/VL53L5CX_FreeRTOS/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -13,6 +13,11 @@ extern osMessageQueueId_t vlQueueHandle;
 
 int  grid[ROWS][COLS];
 
+//Color mapping
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 
 Screen1View::Screen1View()
 {
@@ -72,7 +77,26 @@ void Screen1View::tick_func(){
 				//box[index].setColor(touchgfx::Color::getColorFromRGB(0, 119, 178));
 				//box[index].setColor(touchgfx::Color::getColorFromHSV(grid[c][r], 255, 255));
 				//box[index].setColor(touchgfx::Color::getColorFromHSV(grid[c][r]-rand()%5, 255, 255));//NoizeMC
-				box[index].setColor(touchgfx::Color::getColorFromHSV(Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE * index], 255, 255));//NoizeMC
+
+				int16_t cell_value;
+
+				//Erreors exeptions
+			    if( Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE * index] == 0 ){
+			    	cell_value = 600;
+			    }
+			    else if( Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE * index] == 255 ){
+			    	cell_value = 600;
+			    }else{
+			      cell_value = Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE * index];
+			    }
+
+			    //Noise exeption
+			    if( cell_value > 600) cell_value = 600;
+			    //Hue mapping
+			    cell_value = (int16_t)map(cell_value, 0, 600, 0, 160);
+
+			    //Filling
+				box[index].setColor(touchgfx::Color::getColorFromHSV(cell_value, 255, 255));//NoizeMC
 				box[index].invalidate();
 				index++;
 			}
